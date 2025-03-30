711766663bbf996bf5d39ed9bb415fa0dd468919
amarchino <marchino.alessandro@gmail.com>
2025-03-30 22:03:14 +0200

    Lesson 59

--- starting-project/src/app/tasks/tasks.component.ts
+++ starting-project/src/app/tasks/tasks.component.ts
@@ -1,23 +1,26 @@
 import { Component, computed, input, signal } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
-import { NewTaskData, Task } from './task/task.model';
+import { NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent, NewTaskComponent]
+  imports: [TaskComponent, NewTaskComponent],
 })
 export class TasksComponent {
   id = input.required<string>();
   name = input.required<string>();
   isAddingTask = signal(false);
 
-  selectedUserTasks = computed(() => ([] as Task[]).filter((task) => task.userId === this.id()));
+  selectedUserTasks = computed(() => this.tasksService.getUserTasks(this.id())().filter((task) => task.userId === this.id()));
+
+  constructor(private tasksService: TasksService) {}
 
   onCompleteTask(id: string) {
-    // TODO
+    this.tasksService.removeTask(id);
   }
   onStartAddTask() {
     this.isAddingTask.set(true);
@@ -26,7 +29,7 @@ export class TasksComponent {
     this.isAddingTask.set(false);
   }
   onAddTask(taskData: NewTaskData) {
-    // TODO
+    this.tasksService.addTask(taskData, this.id());
     this.isAddingTask.set(false);
   }
 }
