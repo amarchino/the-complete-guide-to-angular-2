4db12a0f11aecbdb790734fe7dac35899c7e2a18
amarchino <marchino.alessandro@gmail.com>
2025-03-30 19:59:57 +0200

    Lesson 44

--- starting-project/src/app/tasks/tasks.component.ts
+++ starting-project/src/app/tasks/tasks.component.ts
@@ -1,9 +1,11 @@
 import { Component, input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
+  imports: [TaskComponent]
 })
 export class TasksComponent {
   name = input<string>();
