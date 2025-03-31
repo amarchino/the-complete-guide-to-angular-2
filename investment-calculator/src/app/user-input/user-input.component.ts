eca7ca1b8d153ecbbfcb6cadcf150fc245be23b6
amarchino <marchino.alessandro@gmail.com>
2025-03-31 22:09:46 +0200

    Lesson 77

--- investment-calculator/src/app/user-input/user-input.component.ts
+++ investment-calculator/src/app/user-input/user-input.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
