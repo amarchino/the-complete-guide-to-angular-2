0859482fb107489eadb78af15cbcb8769306096f
amarchino <marchino.alessandro@gmail.com>
2025-03-31 22:00:30 +0200

    Lesson 74

---
+++ investment-calculator/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { UserInputComponent } from "./user-input/user-input.component";
+import { InvestmentResultComponent } from "./investment-result/investment-result.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'investment-calculator';
+}
