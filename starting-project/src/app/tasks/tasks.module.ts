b3e9a12673d9ecdf12dfbf4c9f5fb582ee5fdce5
amarchino <marchino.alessandro@gmail.com>
2025-03-31 19:42:12 +0200

    Lesson 73

---
+++ starting-project/src/app/tasks/tasks.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { TasksComponent } from './tasks.component';
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SharedModule
+  ],
+  exports: [
+    TasksComponent,
+  ]
+})
+export class TasksModule { }
