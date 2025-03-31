3576efeff0eb03757d6aaea9967494581cfb666c
amarchino <marchino.alessandro@gmail.com>
2025-03-31 22:19:35 +0200

    Lesson 80

--- investment-calculator/src/app/app.component.ts
+++ investment-calculator/src/app/app.component.ts
@@ -11,4 +11,29 @@ import { InvestmentResultComponent } from "./investment-result/investment-result
 })
 export class AppComponent {
   title = 'investment-calculator';
+
+
+  calculateInvestmentResults(data: { initialInvestment: number, annualInvestment: number, expectedReturn: number, duration: number }) {
+    const { initialInvestment, annualInvestment, duration, expectedReturn } = data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest = investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+
 }
