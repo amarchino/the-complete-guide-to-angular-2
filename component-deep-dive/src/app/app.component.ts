e2f8cde8c5313d80f2681e61ecf8b0b90933c263
amarchino <marchino.alessandro@gmail.com>
2025-04-01 20:40:13 +0200

    Lesson 101

--- component-deep-dive/src/app/app.component.ts
+++ component-deep-dive/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
-import { TraficComponent } from "./dashboard/trafic/trafic.component";
+import { TraficComponent } from "./dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TraficComponent, TicketsComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TraficComponent, TicketsComponent, DashboardItemComponent],
 })
 export class AppComponent {
 
--- component-deep-dive/src/app/dashboard/traffic/traffic.component.ts
+++ component-deep-dive/src/app/dashboard/traffic/traffic.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-trafic',
+  selector: 'app-traffic',
   imports: [],
-  templateUrl: './trafic.component.html',
-  styleUrl: './trafic.component.css'
+  templateUrl: './traffic.component.html',
+  styleUrl: './traffic.component.css'
 })
 export class TraficComponent {
   dummyTrafficData = [
