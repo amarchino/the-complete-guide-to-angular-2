c90085f27912f6f6d5db891eacb2c365f24e275e
amarchino <marchino.alessandro@gmail.com>
2025-04-02 21:18:00 +0200

    Lesson 112

--- component-deep-dive/src/app/shared/button/button.component.ts
+++ component-deep-dive/src/app/shared/button/button.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]',
+  selector: 'button[appButton], a[appButton]',
   imports: [],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.css'
+  styleUrl: './button.component.css',
 })
 export class ButtonComponent {
 
