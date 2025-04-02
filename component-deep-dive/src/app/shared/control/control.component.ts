9da4741e0cd39f0c8a0923bfe8d5d53c725543ca
amarchino <marchino.alessandro@gmail.com>
2025-04-02 21:37:13 +0200

    Lesson 116

--- component-deep-dive/src/app/shared/control/control.component.ts
+++ component-deep-dive/src/app/shared/control/control.component.ts
@@ -7,9 +7,15 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()'
   }
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';
   label = input.required<string>();
+
+  onClick() {
+    console.log('Clicked!');
+  }
 }
