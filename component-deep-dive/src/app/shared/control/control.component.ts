dfcb0b79bfb5502a84e43ca80caec93eb94ef039
amarchino <marchino.alessandro@gmail.com>
2025-04-03 21:50:00 +0200

    Lesson 133

--- component-deep-dive/src/app/shared/control/control.component.ts
+++ component-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
+import { Component, contentChild, ContentChild, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -14,9 +14,11 @@ import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angula
 export class ControlComponent {
   label = input.required<string>();
   private el = inject(ElementRef);
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
 
   onClick() {
     console.log('Clicked!');
     console.log(this.el);
+    console.log(this.control())
   }
 }
