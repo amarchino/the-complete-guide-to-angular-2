a284bb2a674b47910c4be5144a5d0942fbca423c
amarchino <marchino.alessandro@gmail.com>
2025-04-03 23:50:14 +0200

    Lesson 145

--- component-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ component-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, output, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, output, signal, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -11,11 +11,17 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent implements AfterViewInit, OnInit {
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  enteredTitle = signal('');
+  enteredText = signal('');
+
   add = output<{ title: string, text: string }>();
 
-  onSubmit(title: string, text: string) {
-    this.add.emit({ title, text });
-    this.form()?.nativeElement.reset();
+  onSubmit() {
+    this.add.emit({ title: this.enteredTitle(), text: this.enteredText() });
+    this.enteredText.set('');
+    this.enteredTitle.set('');
+    //this.form()?.nativeElement.reset();
   }
 
   ngOnInit(): void {
