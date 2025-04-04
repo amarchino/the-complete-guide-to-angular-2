ca867b5cda015b33fc7b51e0df13e338af055b05
amarchino <marchino.alessandro@gmail.com>
2025-04-04 18:08:51 +0200

    Lesson 147

--- custom-two-way-binding/src/app/rect/rect.component.ts
+++ custom-two-way-binding/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, model, output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -6,11 +6,12 @@ import { Component, input, output } from '@angular/core';
   styleUrl: './rect.component.css',
 })
 export class RectComponent {
-  size = input.required<{ width: string, height: string}>();
-  sizeChange = output<{ width: string, height: string}>();
+  // size = input.required<{ width: string, height: string}>();
+  // sizeChange = output<{ width: string, height: string}>();
+  size = model.required<{ width: string, height: string}>();
 
   onReset() {
-    this.sizeChange.emit({
+    this.size.set({
       width: '200',
       height: '100'
     })
