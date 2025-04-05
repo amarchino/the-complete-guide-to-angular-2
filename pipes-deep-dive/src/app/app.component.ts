a78b55b1f89aee428a6fda7aabeefe23f4dcbb8f
amarchino <marchino.alessandro@gmail.com>
2025-04-05 12:27:09 +0200

    Lesson 170

--- pipes-deep-dive/src/app/app.component.ts
+++ pipes-deep-dive/src/app/app.component.ts
@@ -23,6 +23,7 @@ export class AppComponent {
   ];
 
   onReset(index: number) {
-    this.historicTemperatures[index] = 18;
+    this.historicTemperatures = this.historicTemperatures.map((el, idx) => idx === index ? 18 : el);
+    // this.historicTemperatures[index] = 18;
   }
 }
