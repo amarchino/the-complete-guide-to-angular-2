aa6a91ed408de64185820aa3296152d0fe33adff
amarchino <marchino.alessandro@gmail.com>
2025-04-05 11:54:47 +0200

    Lesson 166

--- pipes-deep-dive/src/app/temperature.pipe.ts
+++ pipes-deep-dive/src/app/temperature.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TemperaturePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return value + ' - transformed';
+  transform(value: string | number, ...args: unknown[]): string {
+    let val: number;
+    if(typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+    const outputTemp = val * (9 / 5) + 32;
+    return `${outputTemp} °F`;
   }
 
 }
