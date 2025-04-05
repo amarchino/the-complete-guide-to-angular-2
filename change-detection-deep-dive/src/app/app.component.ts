ebac2f0614e4387294f5170b0e2938e0bb7f2a06
amarchino <marchino.alessandro@gmail.com>
2025-04-05 20:38:19 +0200

    Lesson 192

---
+++ change-detection-deep-dive/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { CounterComponent } from './counter/counter.component';
+import { MessagesComponent } from './messages/messages.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [CounterComponent, MessagesComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  get debugOutput() {
+    console.log('[AppComponent] "debugOutput" binding re-evaluated.');
+    return 'AppComponent Component Debug Output';
+  }
+}
