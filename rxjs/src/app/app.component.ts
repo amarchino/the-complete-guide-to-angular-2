17d764b239770aabbc7364acfb6a2169f3f5c3b2
amarchino <marchino.alessandro@gmail.com>
2025-04-06 21:06:42 +0200

    Lesson 212

--- rxjs/src/app/app.component.ts
+++ rxjs/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,10 @@ import { toObservable } from '@angular/core/rxjs-interop';
 export class AppComponent implements OnInit {
   clickCount = signal(0);
   clickCount$ = toObservable(this.clickCount);
+
+  interval$ = interval(1000);
+  intervalSignal = toSignal(this.interval$, { initialValue: 0 });
+
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
