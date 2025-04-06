fe86e110cdda73b1ce3f7b38c543f4c8edafa6b0
amarchino <marchino.alessandro@gmail.com>
2025-04-06 21:15:01 +0200

    Lesson 213

--- rxjs/src/app/app.component.ts
+++ rxjs/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { interval } from 'rxjs';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,14 +15,33 @@ export class AppComponent implements OnInit {
   interval$ = interval(1000);
   intervalSignal = toSignal(this.interval$, { initialValue: 0 });
 
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const intervalId = setInterval(() => {
+      if(timesExecuted > 3) {
+        clearInterval(intervalId);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value...');
+      subscriber.next({ message: 'New value' });
+      timesExecuted++;
+    }, 2000);
+  });
+
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     const subscription = this.clickCount$.subscribe({
       next: value => console.log(`Clicked button ${value} times.`)
     });
+    const subscription2 = this.customInterval$.subscribe({
+      next: val => console.log(val),
+      complete: () => console.log('Completed!')
+    });
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
+      subscription2.unsubscribe();
     })
   }
 
