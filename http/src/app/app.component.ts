434a6737603b3722c000c9d2263d922536de6122
amarchino <marchino.alessandro@gmail.com>
2025-04-06 21:26:39 +0200

    Lesson 216

---
+++ http/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AvailablePlacesComponent } from './places/available-places/available-places.component';
+import { UserPlacesComponent } from './places/user-places/user-places.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [AvailablePlacesComponent, UserPlacesComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+}
