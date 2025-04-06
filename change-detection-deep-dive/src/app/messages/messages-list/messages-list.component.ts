7db8310ac31505f73dc52588b0a50c09d35afe32
amarchino <marchino.alessandro@gmail.com>
2025-04-06 12:29:10 +0200

    Lesson 202

--- change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
+++ change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, input, OnDestroy, OnInit } from '@angular/core';
 import { MessagesService } from '../messages.service';
 
 @Component({
@@ -8,12 +8,21 @@ import { MessagesService } from '../messages.service';
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessagesListComponent {
+export class MessagesListComponent implements OnInit {
   private messagesService = inject(MessagesService);
-  get messages() {
-    return this.messagesService.allMessages;
+  private cdRef = inject(ChangeDetectorRef);
+  private destroyRef = inject(DestroyRef);
+  messages: string[] = [];
+
+  ngOnInit(): void {
+    const subscription = this.messagesService.messages$.subscribe(messages => {
+      this.messages = messages;
+      this.cdRef.markForCheck();
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
-  //messages = this.messagesService.allMessages;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
