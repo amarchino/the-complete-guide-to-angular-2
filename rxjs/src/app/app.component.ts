eb0ba723f3f95d9928025e83722c6d5d6f7c4ae4
amarchino <marchino.alessandro@gmail.com>
2025-04-06 20:38:49 +0200

    Lesson 208

---
+++ rxjs/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = interval(1000).subscribe({
+      next: val => console.log(val)
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+}
