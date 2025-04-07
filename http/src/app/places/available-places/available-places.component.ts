a2f856adc4d6a81524323def24342bda58066b35
amarchino <marchino.alessandro@gmail.com>
2025-04-07 19:15:50 +0200

    Lesson 228

--- http/src/app/places/available-places/available-places.component.ts
+++ http/src/app/places/available-places/available-places.component.ts
@@ -3,8 +3,8 @@ import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
-import { HttpClient } from '@angular/common/http';
 import { catchError, map, throwError } from 'rxjs';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-available-places',
@@ -17,19 +17,13 @@ export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
   error = signal('');
-  private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
+
+  private placesService = inject(PlacesService);
 
   ngOnInit(): void {
     this.isFetching.set(true);
-    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places')
-      .pipe(
-        map(resData => resData.places),
-        catchError(error => {
-          console.log(error);
-          return throwError(() => new Error('Something went wrong fetching the available places. Please try again later.'));
-        })
-      )
+    const subscription = this.placesService.loadAvailablePlaces()
       .subscribe({
         next: places => {
           this.places.set(places);
@@ -47,9 +41,12 @@ export class AvailablePlacesComponent implements OnInit {
   }
 
   onSelectPlace(selectedPlace: Place) {
-    this.httpClient.put('http://localhost:3000/user-places', { placeId: selectedPlace.id })
+    const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace.id)
       .subscribe({
         next: resData => console.log(resData)
       });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 }
