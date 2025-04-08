74128f0f96f7c6975d2f1d7180106d2d11ac1fe1
amarchino <marchino.alessandro@gmail.com>
2025-04-08 22:23:59 +0200

    Lesson 237

--- http/src/app/app.config.ts
+++ http/src/app/app.config.ts
@@ -1,12 +1,21 @@
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
   const req = request.clone({
     headers: request.headers.set('X-DEBUG', 'TESTING')
   });
   console.log('[Outgoing Request]', req);
-  return next(req);
+  return next(req).pipe(
+    tap({
+      next: event => {
+        if(event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]', event.status, event.body);
+        }
+      }
+    })
+  );
 }
 
 export const appConfig: ApplicationConfig = {
