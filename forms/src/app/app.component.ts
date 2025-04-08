cf6ffac542b25648204de41a1933f5715b65b079
amarchino <marchino.alessandro@gmail.com>
2025-04-08 22:29:28 +0200

    Lesson 238

---
+++ forms/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LoginComponent } from "./auth/login/login.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [LoginComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'forms';
+}
