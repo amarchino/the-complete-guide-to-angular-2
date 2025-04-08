63bc9c1aa9a3b8d7e756b0a6180177a7aafc667d
amarchino <marchino.alessandro@gmail.com>
2025-04-08 22:20:40 +0200

    Lesson 235

--- http/src/app/app.config.ts
+++ http/src/app/app.config.ts
@@ -1,10 +1,19 @@
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+  const req = request.clone({
+    headers: request.headers.set('X-DEBUG', 'TESTING')
+  });
+  console.log('[Outgoing Request]', req);
+  return next(req);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([
+      loggingInterceptor
+    ]))
   ]
 };
