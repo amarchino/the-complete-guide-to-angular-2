fd62262a9013a244f62a002288d42122282b54b0
amarchino <marchino.alessandro@gmail.com>
2025-04-08 22:15:11 +0200

    Lesson 234

--- http/src/app/places/user-places/user-places.component.ts
+++ http/src/app/places/user-places/user-places.component.ts
@@ -3,6 +3,7 @@ import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
 import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
 
 @Component({
   selector: 'app-user-places',
@@ -34,4 +35,21 @@ export class UserPlacesComponent implements OnInit {
       subscription.unsubscribe();
     });
   }
+
+  onSelectPlace(place: Place) {
+    this.isFetching.set(true);
+    const subscription = this.placesService.removeUserPlace(place)
+      .subscribe({
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        }
+      });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
 }
