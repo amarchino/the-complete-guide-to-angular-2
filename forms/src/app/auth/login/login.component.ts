938b56237d091e3457da4ebc20b33934f420c5b8
amarchino <marchino.alessandro@gmail.com>
2025-04-09 21:44:28 +0200

    Lesson 242

--- forms/src/app/auth/login/login.component.ts
+++ forms/src/app/auth/login/login.component.ts
@@ -9,7 +9,9 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class LoginComponent {
 
-  onSubmit(form: NgForm) {
-    console.log(form);
+  onSubmit(formData: NgForm) {
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
+    console.log(enteredEmail, enteredPassword)
   }
 }
