2fbc5c6c828689e0927991ad76396288931c8ff5
amarchino <marchino.alessandro@gmail.com>
2025-04-10 21:29:31 +0200

    Lesson 249

--- forms/src/app/auth/login/login.component.ts
+++ forms/src/app/auth/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
