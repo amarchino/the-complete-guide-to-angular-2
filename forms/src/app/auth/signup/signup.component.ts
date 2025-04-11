0683d8afe6cf06f23f1f21219079b7107c2b77a9
amarchino <marchino.alessandro@gmail.com>
2025-04-11 15:41:12 +0200

    Lesson 258

--- forms/src/app/auth/signup/signup.component.ts
+++ forms/src/app/auth/signup/signup.component.ts
@@ -14,18 +14,22 @@ export class SignupComponent {
     email: new FormControl('', {
       validators: [ Validators.required, Validators.email ]
     }),
-    password: new FormControl('', {
-      validators: [ Validators.required ]
-    }),
-    confirmPassword: new FormControl('', {
-      validators: [ Validators.required ]
+    passwords: new FormGroup({
+      password: new FormControl('', {
+        validators: [ Validators.required ]
+      }),
+      confirmPassword: new FormControl('', {
+        validators: [ Validators.required ]
+      }),
     }),
     firstName: new FormControl('', { validators: [ Validators.required ] }),
     lastName: new FormControl('', { validators: [ Validators.required ] }),
-    street: new FormControl('', { validators: [ Validators.required ] }),
-    number: new FormControl('', { validators: [ Validators.required ] }),
-    postalCode: new FormControl('', { validators: [ Validators.required ] }),
-    city: new FormControl('', { validators: [ Validators.required ] }),
+    address: new FormGroup({
+      street: new FormControl('', { validators: [ Validators.required ] }),
+      number: new FormControl('', { validators: [ Validators.required ] }),
+      postalCode: new FormControl('', { validators: [ Validators.required ] }),
+      city: new FormControl('', { validators: [ Validators.required ] })
+    }),
     role: new FormControl<Role>('student', { validators: [ Validators.required ] }),
     agree: new FormControl(false, { validators: [ Validators.required] })
   });
