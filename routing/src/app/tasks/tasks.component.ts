b44b15588a0efbdfc5a609a4c8fe7c705516f215
amarchino <marchino.alessandro@gmail.com>
2025-04-12 21:12:37 +0200

    Lesson 301

--- routing/src/app/tasks/tasks.component.ts
+++ routing/src/app/tasks/tasks.component.ts
@@ -1,8 +1,7 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
-import { TasksService } from './tasks.service';
-import { ActivatedRouteSnapshot, ResolveFn, RouterLink, RouterStateSnapshot } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { Task } from './task/task.model';
 
 @Component({
@@ -15,23 +14,4 @@ export class TasksComponent {
   userTasks = input.required<Task[]>();
   userId = input.required<string>();
   order = input<'asc' | 'desc' | undefined>('desc');
-}
-
-export const resolveUserTasks: ResolveFn<Task[]> = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-  const order = route.queryParams['order'];
-  const userId = route.paramMap.get('userId');
-  const tasksService = inject(TasksService);
-  const tasks = tasksService
-    .allTasks()
-    .filter(task => task.userId === userId)
-    .sort((a, b) => {
-      if (a.id === b.id) {
-        return 0;
-      }
-      if(order === 'desc' || order === undefined) {
-        return a.id > b.id ? -1 : 1;
-      }
-      return a.id > b.id ? 1 : -1;
-    });
-  return tasks.length ? tasks : [];
 }
--- routing/src/app/users/users.routes.ts
+++ routing/src/app/users/users.routes.ts
@@ -1,6 +1,28 @@
-import { Routes } from '@angular/router';
-import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
+import { ResolveFn, Routes } from '@angular/router';
 import { canLeaveEditPage, NewTaskComponent } from '../tasks/new-task/new-task.component';
+import { Task } from '../tasks/task/task.model';
+import { TasksService } from '../tasks/tasks.service';
+import { inject } from '@angular/core';
+
+export const resolveUserTasks: ResolveFn<Task[]> = (route) => {
+  const order = route.queryParams['order'];
+  const userId = route.paramMap.get('userId');
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(task => task.userId === userId)
+    .sort((a, b) => {
+      if (a.id === b.id) {
+        return 0;
+      }
+      if(order === 'desc' || order === undefined) {
+        return a.id > b.id ? -1 : 1;
+      }
+      return a.id > b.id ? 1 : -1;
+    });
+  return tasks.length ? tasks : [];
+}
+
 
 export const routes: Routes = [
   {
@@ -10,7 +32,7 @@ export const routes: Routes = [
   },
   {
     path: 'tasks',
-    component: TasksComponent,
+    loadComponent: () => import('../tasks/tasks.component').then(mod => mod.TasksComponent),
     runGuardsAndResolvers: 'always',
     resolve: {
       userTasks: resolveUserTasks
