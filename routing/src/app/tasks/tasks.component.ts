c8a2452f35568e5f9848518f03f6b38eb6661146
amarchino <marchino.alessandro@gmail.com>
2025-04-12 17:46:10 +0200

    Lesson 283

--- routing/src/app/tasks/tasks.component.ts
+++ routing/src/app/tasks/tasks.component.ts
@@ -3,12 +3,13 @@ import { Component, computed, inject, input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink ],
 })
 export class TasksComponent {
   private readonly tasksService = inject(TasksService);
