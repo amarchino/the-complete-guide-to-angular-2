15c20bf16786e87fa08337a8c2e81c9f87232c22
amarchino <marchino.alessandro@gmail.com>
2025-04-12 19:51:47 +0200

    Lesson 297

--- routing/src/app/users/users.routes.ts
+++ routing/src/app/users/users.routes.ts
@@ -3,11 +3,15 @@ import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
 import { canLeaveEditPage, NewTaskComponent } from '../tasks/new-task/new-task.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full'
+  },
   {
     path: 'tasks',
     component: TasksComponent,
-    runGuardsAndResolvers: 'paramsOrQueryParamsChange',
+    runGuardsAndResolvers: 'always',
     resolve: {
       userTasks: resolveUserTasks
     }
