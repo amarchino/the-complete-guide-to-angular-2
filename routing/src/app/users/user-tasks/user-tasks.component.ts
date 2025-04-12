c6462a164074c0a507b5ba220934fcbad844768b
amarchino <marchino.alessandro@gmail.com>
2025-04-12 16:03:24 +0200

    Lesson 271

--- routing/src/app/users/user-tasks/user-tasks.component.ts
+++ routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user-tasks',
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  private readonly usersService = inject(UsersService);
+  userId = input.required<string>();
+
+  userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name);
+
+}
