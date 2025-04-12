ca0faddbd2d866783eefd8e2c46ba61d72ed0aa7
amarchino <marchino.alessandro@gmail.com>
2025-04-12 15:41:19 +0200

    Lesson 266

--- routing/src/app/app.component.ts
+++ routing/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UsersComponent } from './users/users.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UsersComponent],
+  imports: [HeaderComponent, UsersComponent, RouterModule],
 })
 export class AppComponent {}
