b617762160736d8010f57670d88c7a6612d8465e
amarchino <marchino.alessandro@gmail.com>
2025-04-12 16:35:34 +0200

    Lesson 274

--- routing/src/app/users/user-tasks/user-tasks.component.ts
+++ routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,31 +1,17 @@
-import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { UsersService } from '../users.service';
-import { ActivatedRoute } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
+  imports: [ RouterOutlet ],
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent implements OnInit {
+export class UserTasksComponent {
   private readonly usersService = inject(UsersService);
-  // private readonly activatedRoute = inject(ActivatedRoute);
-  // private readonly destroyRef = inject(DestroyRef);
   userId = input.required<string>();
 
   userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name);
-  // userNameViaObservables = '';
-
-  ngOnInit(): void {
-    // console.log(this.activatedRoute);
-    // const subsciption = this.activatedRoute.paramMap.subscribe({
-    //   next: paramMap => {
-    //     this.userNameViaObservables = this.usersService.users.find(u => u.id === paramMap.get('userId'))?.name ?? '';
-    //   }
-    // });
-    // this.destroyRef.onDestroy(() => {
-    //   subsciption.unsubscribe();
-    // })
-  }
 
 }
