2691ef2334ccdab135a094257090e8838b11d16d
amarchino <marchino.alessandro@gmail.com>
2025-04-13 20:41:16 +0200

    Lesson 316

--- routing/src/app/app.config.ts
+++ routing/src/app/app.config.ts
@@ -2,10 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withDebugTracing, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding(), withRouterConfig({ paramsInheritanceStrategy: 'always' }))
+    provideRouter(routes, withComponentInputBinding(), withRouterConfig({ paramsInheritanceStrategy: 'always' })),
+    provideClientHydration(withEventReplay())
   ]
 };
