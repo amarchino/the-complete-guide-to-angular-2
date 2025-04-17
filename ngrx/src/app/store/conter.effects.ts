c128faa3872f12f0c7e6f11fd229c74a5a7d7224
amarchino <marchino.alessandro@gmail.com>
2025-04-17 21:36:27 +0200

    Lesson 697

--- ngrx/src/app/store/conter.effects.ts
+++ ngrx/src/app/store/conter.effects.ts
@@ -1,17 +1,21 @@
 import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { decrement, increment } from './counter.actions';
-import { tap } from 'rxjs';
+import { tap, withLatestFrom } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectCount } from './counter.selectors';
 
 @Injectable()
 export class CounterEffects {
   private readonly actions$ = inject(Actions);
+  private readonly store = inject(Store);
 
   saveCount = createEffect(() => this.actions$.pipe(
     ofType(increment, decrement),
-    tap((action) => {
-      console.log(action);
-      localStorage.setItem('count', action.value.toString());
+    withLatestFrom(this.store.select(selectCount)),
+    tap(([action, counter]) => {
+      console.log(action, counter);
+      localStorage.setItem('count', counter.toString());
     })
   ), { dispatch: false });
 }
