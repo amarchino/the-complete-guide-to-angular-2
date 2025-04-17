1dbc5545f7201d3daae84ecd9cf3b413fbf0e800
amarchino <marchino.alessandro@gmail.com>
2025-04-17 20:44:56 +0200

    Lesson 687

--- ngrx/src/app/counter-controls/counter-controls.component.ts
+++ ngrx/src/app/counter-controls/counter-controls.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { CounterService } from '../counter.service';
+import { Store } from '@ngrx/store';
+import { decrement, increment } from '../store/counter.actions';
 
 @Component({
   selector: 'app-counter-controls',
@@ -7,13 +8,13 @@ import { CounterService } from '../counter.service';
   styleUrls: ['./counter-controls.component.css'],
 })
 export class CounterControlsComponent {
-  private readonly counterService = inject(CounterService);
+  private readonly store = inject(Store);
 
   increment() {
-    this.counterService.increment();
+    this.store.dispatch(increment());
   }
 
   decrement() {
-    this.counterService.decrement();
+    this.store.dispatch(decrement());
   }
 }
