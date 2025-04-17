4c2ab01af3693b3240aed553216079649b1d8c6c
amarchino <marchino.alessandro@gmail.com>
2025-04-17 21:19:18 +0200

    Lesson 694

--- ngrx/src/app/app.config.ts
+++ ngrx/src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './store/counter.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideStore({
-    counter: counterReducer
-  })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideStore({
+        counter: counterReducer
+    }),
+    provideEffects()
+  ]
 };
