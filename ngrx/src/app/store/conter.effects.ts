96c90d3ce1e50649209197d2ee1c69e383247e75
amarchino <marchino.alessandro@gmail.com>
2025-04-17 21:27:08 +0200

    Lesson 695

---
+++ ngrx/src/app/store/conter.effects.ts
@@ -0,0 +1,16 @@
+import { inject } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { decrement, increment } from './counter.actions';
+import { tap } from 'rxjs';
+
+export class CounterEffects {
+  private readonly actions$ = inject(Actions);
+
+  saveCount = createEffect(() => this.actions$.pipe(
+    ofType(increment, decrement),
+    tap((action) => {
+      console.log(action);
+      localStorage.setItem('count', action.value.toString());
+    })
+  ), { dispatch: false });
+}
